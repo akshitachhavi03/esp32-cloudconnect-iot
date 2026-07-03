7de642f079a4ffea0ec40c2e1842744ba1c8650b
akshitachhavi <akshitachhavi1802@gmail.com>
2026-07-03 22:25:34 +0530

    implemented wifi connection

--- sketch.ino
+++ sketch.ino
@@ -5,8 +5,21 @@ const char* password = "";
 
 void setup() {
   Serial.begin(115200);
+
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
 }
